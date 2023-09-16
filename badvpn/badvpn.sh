35d16da9e6f35b653e4fa673d1aacd231f7544a5
netfreeplus <137662776+netfreeplus@users.noreply.github.com>
2023-09-16 18:10:44 -0500

    Update badvpn.sh

--- badvpn/badvpn.sh
+++ badvpn/badvpn.sh
@@ -51,8 +51,8 @@ fun_udp1 () {
 	    inst_udp () {
 	        cd $HOME
 			apt-get install dos2unix -y
-            wget https://raw.githubusercontent.com/thefather12/SSHPLUS/main/badvpn/badvpn-udpgw -o /dev/null
-			wget https://raw.githubusercontent.com/thefather12/SSHPLUS/main/badvpn/antcrashvpn.sh -o /dev/null
+            wget https://raw.githubusercontent.com/netfreeplus/SSHPLUS/main/badvpn/badvpn-udpgw -o /dev/null
+			wget https://raw.githubusercontent.com/netfreeplus/SSHPLUS/main/badvpn/antcrashvpn.sh -o /dev/null
 			dos2unix antcrashvpn.sh
 			mv -f $HOME /antcrashvpn.sh /bin/antcrashvpn.sh
             mv -f $HOME/badvpn-udpgw /bin/badvpn-udpgw
